0243199e3a408f997667c81da438e6466e09d15d
lowkey3Dfx <Arif.Kesdiren@gmail.com>
2023-01-12 15:40:56 +0100

    Add update
    logged each imgLink to console

--- index.js
+++ index.js
@@ -6,19 +6,26 @@ const PORT = 8000;
 const app = express();
 
 const url = 'https://memegen-link-examples-upleveled.netlify.app';
-
+// axios fetch data from url
 axios(url)
   .then((response) => {
+    // extract html
     const html = response.data;
     const $ = cheerio.load(html);
     const memesArray = [];
-
+    // looking inside div and section html to find img and src attributes
     $('div', 'section', html).each(function () {
+      // store links inside imgSrc variable and push links inside empty memesArray variable
       const imgSrc = $(this).find('img').attr('src');
       memesArray.push({ imgSrc });
     });
+    // store first 10 links inside memes variable
     const memes = memesArray.slice(0, 10);
-    console.log(memes);
+    // create new array only containing the img src links using map method
+    const mapLinks = memes.map((mem) => mem.imgSrc);
+    // log each link
+    const eachLink = mapLinks.forEach((link) => console.log(link));
+    // console.log(mapLinks);
   })
   .catch((err) => console.log(err));
 
